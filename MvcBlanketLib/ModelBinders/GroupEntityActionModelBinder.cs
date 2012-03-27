eb205e35d70fc0b2c4e412f7ade4ce5f17b37f5e
Leonid Gordo <leonardpt@gmail.com>
2012-03-27 22:18:21 +0400

    add GroupEntityActionModelBinder

---
+++ MvcBlanketLib/ModelBinders/GroupEntityActionModelBinder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace MvcBlanketLib.ModelBinders
+{
+    public class GroupEntityActionModelBinder<T> : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var formData = controllerContext.HttpContext.Request.Form;
+            var action = formData["action"];
+            var ids = formData["chk"].Split(',').Where(s => !string.IsNullOrWhiteSpace(s));
+            var groupAction = new GroupAction<T> { Action = action, Identifiers = ids.Select(i => (T)Convert.ChangeType(i, typeof(T))) };
+            return groupAction;
+        }
+    }
+
+    public interface IGroupAction
+    {
+
+    }
+
+    public class GroupAction<T> : IGroupAction
+    {
+        public string Action { get; internal set; }
+        public IEnumerable<T> Identifiers { get; internal set; }
+    }
+}
---
+++ MvcBlanketLib/ModelBinders/GroupEntityActionModelBinderProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace MvcBlanketLib.ModelBinders
+{
+    public class GroupEntityActionModelBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder GetBinder(Type modelType)
+        {
+            if (!typeof(IGroupAction).IsAssignableFrom(modelType))
+                return null;
+            var genericType = modelType.GetGenericArguments().First();
+            var modelBinderType = typeof(GroupEntityActionModelBinder<>).MakeGenericType(genericType);
+            var modelBinder = Activator.CreateInstance(modelBinderType);
+            return (IModelBinder)modelBinder;
+
+        }
+    }
+
+}
