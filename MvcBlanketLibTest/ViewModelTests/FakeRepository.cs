f9224698049ca75a2e37d27b1290ba89481108b6
Leonid Gordo <leonardpt@gmail.com>
2012-05-10 15:58:31 +0400

    add tests for paged view model and paged view model factory

---
+++ MvcBlanketLibTest/ViewModelTests/FakeRepository.cs
@@ -0,0 +1,54 @@
+﻿/*
+MVC Blanket Library Copyright (C) 2009-2012 Leonid Gordo
+
+This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; 
+either version 3.0 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with this library; 
+if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MvcBlanketLib.PageFilters;
+
+namespace MvcBlanketLibTest.ViewModelTests
+{
+    public class FakeRepository
+    {
+        private readonly IEnumerable<FakeEntity> entities;
+        public IEnumerable<FakeEntity> Entities { get { return entities; }}
+
+        public FakeRepository()
+        {
+            entities = new List<FakeEntity>
+                           {
+                               new FakeEntity { Id = 1, Title = "Book", Price = 10, Status = true, Published = new DateTime(2000, 01, 01)},
+                               new FakeEntity { Id = 2, Title = "Lamp", Price = 20, Status = true, Published = new DateTime(2000, 06, 01)},
+                               new FakeEntity { Id = 3, Title = "Pen", Price = 5, Status = false, Published = new DateTime(2001, 01, 01)},
+                               new FakeEntity { Id = 4, Title = "Notebook", Price = 7, Status = true, Published = new DateTime(2002, 01, 01)},
+                               new FakeEntity { Id = 5, Title = "Table", Price = 100, Status = true, Published = new DateTime(1999, 01, 01)},
+                               new FakeEntity { Id = 6, Title = "Pencil", Price = 2, Status = true, Published = new DateTime(2001, 04, 01)}
+                           };
+        }
+
+        public IQueryable<FakeEntity> Get (FakeFiltersModel filters)
+        {
+            var query = Entities.AsQueryable();
+            if (filters == null) return query;
+            query = query.Where(filters.Title, c => c.Title.StartsWith(filters.Title.Value))
+                .Where(filters.Price, c => c.Price <= filters.Price.Value)
+                .Where(filters.Status, c => c.Status == filters.Status.Value)
+                .Where(filters.Published,
+                       c =>
+                       c.Published >= filters.Published.Value.LowerBound &&
+                       c.Published <= filters.Published.Value.UpperBound);
+            return query;
+
+        }
+    }
+}
---
+++ MvcBlanketLibTest/ViewModelTests/PagedViewModelFactoryTests.cs
@@ -0,0 +1,84 @@
+﻿/*
+MVC Blanket Library Copyright (C) 2009-2012 Leonid Gordo
+
+This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; 
+either version 3.0 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with this library; 
+if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+*/
+
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MvcBlanketLib.ViewModels;
+using MvcContrib.Sorting;
+using MvcContrib.UI.Grid;
+
+namespace MvcBlanketLibTest.ViewModelTests
+{
+    [TestClass]
+    public class PagedViewModelFactoryTests
+    {
+
+        private readonly ViewDataDictionary viewData = new ViewDataDictionary();
+        private readonly Mock<ControllerContext> controllerContext = new Mock<ControllerContext>();
+        private readonly Mock<Controller> controller = new Mock<Controller>();
+
+        public PagedViewModelFactoryTests()
+        {
+            controllerContext.Setup(m => m.Controller).Returns(controller.Object);
+        }
+
+
+        [TestMethod]
+        public void TestCreateModelFromViewData()
+        {
+            viewData["PageNumber"] = 2;
+            viewData["PageSize"] = 2;
+            var gso = new GridSortOptions {Column = "Published", Direction = SortDirection.Ascending};
+            viewData["GridSortOptions"] = gso;
+
+
+            var model =
+                PagedViewModelFactory.Create<FakeEntity>(viewData).Apply(
+                    f => new FakeRepository().Get(f as FakeFiltersModel)).Setup();
+
+            Assert.IsNotNull(model);
+            Assert.IsNotNull(model.PagedList);
+            Assert.AreEqual(2, model.Page);
+            Assert.AreEqual(2, model.PageSize);
+            Assert.AreEqual(6, model.PagedList.TotalItems);
+            Assert.AreEqual(3, model.PagedList.TotalPages);
+            Assert.AreEqual(2, model.PagedList.ElementAt(0).Id);
+            Assert.AreEqual(3, model.PagedList.ElementAt(1).Id);
+        }
+
+        [TestMethod]
+        public void TestCreateModelFromContext()
+        {
+            controller.Object.ViewBag.PageNumber = 2;
+            controller.Object.ViewBag.PageSize = 2;
+            var gso = new GridSortOptions { Column = "Published", Direction = SortDirection.Ascending };
+            controller.Object.ViewBag.GridSortOptions = gso;
+
+
+            var model =
+                PagedViewModelFactory.Create<FakeEntity>(controllerContext.Object).Apply(
+                    f => new FakeRepository().Get(f as FakeFiltersModel)).Setup();
+
+            Assert.IsNotNull(model);
+            Assert.IsNotNull(model.PagedList);
+            Assert.AreEqual(2, model.Page);
+            Assert.AreEqual(2, model.PageSize);
+            Assert.AreEqual(6, model.PagedList.TotalItems);
+            Assert.AreEqual(3, model.PagedList.TotalPages);
+            Assert.AreEqual(2, model.PagedList.ElementAt(0).Id);
+            Assert.AreEqual(3, model.PagedList.ElementAt(1).Id);
+        }
+    }
+}
