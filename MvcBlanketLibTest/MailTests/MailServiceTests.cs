6d2ae50260865f2900d103c3f900d51f7d2e3270
Leonid Gordo <leonardpt@gmail.com>
2012-05-10 17:56:06 +0400

    add tests for Mail service

---
+++ MvcBlanketLibTest/MailTests/MailServiceTests.cs
@@ -0,0 +1,101 @@
+﻿/*
+MVC Blanket Library Copyright (C) 2009-2012 Leonid Gordo
+
+This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; 
+either version 3.0 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with this library; 
+if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+*/
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcBlanketLib.Mail;
+using Moq;
+
+namespace MvcBlanketLibTest.MailTests
+{
+    [TestClass]
+    public class MailServiceTests
+    {
+
+        const string RecipientEmail = "test@mail.com";
+        const string TemplateName = "TestTemplate";
+
+        private readonly Mock<IMailStorage> mailStorage = new Mock<IMailStorage>();
+
+        [TestMethod]
+        public void TestRegisterMail()
+        {
+            var storage = mailStorage.Object;
+            var mailService = MailService.Instance.RegisterStorage(storage).RegisterAssembly(this.GetType().Assembly);
+            var mail = mailService.RegisterMail(RecipientEmail, TemplateName);
+            Assert.IsNotNull(mail);
+        }
+
+        [TestMethod]
+        public void TestAddVariableToMail()
+        {
+            var storage = mailStorage.Object;
+            var mailService = MailService.Instance.RegisterStorage(storage).RegisterAssembly(this.GetType().Assembly);
+            var mail = mailService.RegisterMail(RecipientEmail, TemplateName);
+            mail.AddVariable("StringVariable", "StringValue").AddVariable("IntVariable", 10).AddVariable(
+                "BoolVariable", true);
+            Assert.IsNotNull(mail);
+            Assert.AreEqual(4, mail.Variables.Count());
+            Assert.AreEqual("StringValue", mail.Variables["StringVariable"]);
+            Assert.AreEqual(10, mail.Variables["IntVariable"]);
+            Assert.AreEqual(true, mail.Variables["BoolVariable"]);
+            Assert.IsTrue(mail.Variables.ContainsKey("Domain"));
+        }
+
+        [TestMethod]
+        public void TestSaveMailToPipeline()
+        {
+            var storage = mailStorage.Object;
+            var mailService = MailService.Instance.RegisterStorage(storage).RegisterAssembly(this.GetType().Assembly);
+            var mail = mailService.RegisterMail(RecipientEmail, TemplateName);
+            mail.AddVariable("StringVariable", "StringValue").AddVariable("IntVariable", 10).AddVariable(
+                "BoolVariable", true).Save();
+            mailStorage.Verify(m => m.SerializeMail(mail));
+        }
+
+        [TestMethod]
+        public void TestMailServiceProcessEmptyMailQueue()
+        {
+            var storage = mailStorage.Object;
+            var mailService = MailService.Instance.RegisterStorage(storage).RegisterAssembly(this.GetType().Assembly);
+            MailService.Instance.ProcessQueue();
+            mailStorage.Verify(m => m.DeserializeMail());
+        }
+
+        [TestMethod]
+        public void TestMailServiceProcessMailQueueWithOneMail()
+        {
+            var calls = 0;
+            mailStorage.Setup(m => m.DeserializeMail()).Returns(() => calls == 0
+                                                                          ? new Mail()
+                                                                                {
+                                                                                    RecipientEmail = RecipientEmail,
+                                                                                    TemplateName = TemplateName,
+                                                                                    MailId = Guid.NewGuid(),
+                                                                                    Storage = mailStorage.Object,
+                                                                                    Variables =
+                                                                                        new Dictionary<string, object>()
+                                                                                }
+                                                                          : null)
+                                                                          .Callback(() => calls++);
+            mailStorage.SetupGet(m => m.TemplatesDirectory).Returns("~/MailTests");
+            var storage = mailStorage.Object;
+            var mailService = MailService.Instance.RegisterStorage(storage).RegisterAssembly(this.GetType().Assembly);
+            MailService.Instance.ProcessQueue();
+            mailStorage.Verify(m => m.DeserializeMail(), Times.Exactly(2));
+        }
+    }
+}
